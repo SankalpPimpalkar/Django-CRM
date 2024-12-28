f7d38d350854dda3cbeeaccb706fc6120eaf3951
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2024-12-29 01:37:49 +0530

    Add leads app and defined Lead and Agent Model

---
+++ leads/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Lead(models.Model):
+    
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField(default=0)
+    agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
