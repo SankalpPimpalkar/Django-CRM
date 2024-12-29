79c8665d21b709dafb9bfa719cc0c4219d57870e
Sankalp Pimpalkar <pimpalkarsankalp8@gmail.com>
2024-12-29 22:09:34 +0530

    Configured Admin Panel of Django

--- leads/models.py
+++ leads/models.py
@@ -1,13 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    pass
+
 class Lead(models.Model):
-    
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey('Agent', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Agent(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    user = models.OneToOneField('CustomUser', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.email
